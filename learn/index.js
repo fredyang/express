cfe1637083c8ae39e68bc86a377c99506505310f
Fred Yang <fredyang@semanticsworks.com>
2015-11-10 16:12:24 -0500

    SAVEPOINT

---
+++ learn/index.js
@@ -0,0 +1,11 @@
+var express = require('../lib/express');
+
+var app = express();
+app.use(function (req, res) {
+    res.end('bye');
+});
+
+
+app.listen(3000);
+console.log('listening on port 3000');
+

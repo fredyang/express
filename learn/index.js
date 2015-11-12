2a111e4e2a0c4f63b6a0c53c5bfe21fa27f68429
Fred Yang <fredyang@semanticsworks.com>
2015-11-12 00:18:18 -0500

    SAVEPOINT

--- learn/index.js
+++ learn/index.js
@@ -1,6 +1,24 @@
 var express = require('../lib/express');
+var cookieParser = require('cookie-parser');
 
 var app = express();
+app.use(cookieParser());
+
+app.get('/', function(req, res){
+    res.cookie('cookie', 'hey');
+    res.send();
+});
+
+app.get('/return', function(req, res){
+    if (req.cookies.cookie) {
+        res.send(req.cookies.cookie);
+    }
+    else {
+        res.send(':(')
+    }
+});
+
+
 app.use(function (req, res) {
     res.end('bye');
 });
--- learn/index_spec.js
+++ learn/index_spec.js
@@ -1,9 +1,26 @@
 var app = require('./index');
-var request = require('supertest')(app);
+var supertest = require('supertest');
+//var request = supertest.agent(app);
+//var request = supertest.agent(app);
+//looks there is no difference for these two approach
+var request = supertest(app);
 
 describe('index', function () {
-    it('should be ok', function (done) {
-        request.get('/')
-            .expect(200, 'bye', done);
+    //it('should be ok', function (done) {
+    //    request.get('/')
+    //        .expect(200, 'bye', done);
+    //});
+
+    it('should save cookies', function(done){
+        request
+            .get('/')
+            .expect('set-cookie', 'cookie=hey; Path=/', done);
     });
+
+    it('should send cookies', function(done){
+        request
+            .get('/return')
+            .expect('hey', done);
+    });
+
 });
